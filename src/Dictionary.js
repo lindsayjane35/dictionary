caa1284a797310ac3bfe7cb347a3f2037f760cb6
Lindsay <lindsayjane35@gmail.com>
2024-03-31 19:59:14 -0700

    added phonetics

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,19 +4,17 @@ import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-  let [keyWord, setKeyword] = useState("");
+  let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
     setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`searching for ${keyWord}`);
 
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyWord}`;
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
